a43d843f78084a716dd24bcbb53979424fbe1cce
Влад Потук <vladpotuk01@gmail.com>
2024-07-05 20:51:31 +0300

    update

--- DogApp/MainWindow.xaml.cs
+++ DogApp/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Windows;
@@ -11,26 +12,98 @@ namespace DogApp
 {
     public partial class MainWindow : Window
     {
+        private HttpClient client = new HttpClient();
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private async void Button_Click(object sender, RoutedEventArgs e)
+        private async void GetRandomDog_Click(object sender, RoutedEventArgs e)
         {
             string url = "https://dog.ceo/api/breeds/image/random";
-            HttpClient client = new HttpClient();
+            await FetchAndDisplayImage(url);
+        }
 
+        private async void ListAllBreeds_Click(object sender, RoutedEventArgs e)
+        {
+            string url = "https://dog.ceo/api/breeds/list/all";
             HttpResponseMessage response = await client.GetAsync(url);
             string responseData = await response.Content.ReadAsStringAsync();
 
             JObject json = JObject.Parse(responseData);
-            string xmlData = JsonConvert.DeserializeXmlNode(json.ToString(), "Root").OuterXml;
+            var breeds = json["message"].ToObject<Dictionary<string, List<string>>>();
 
-            XmlDocument xmlDoc = new XmlDocument();
-            xmlDoc.LoadXml(xmlData);
+            breedList.Items.Clear();
+            foreach (var breed in breeds.Keys)
+            {
+                breedList.Items.Add(breed);
+            }
+        }
 
-            string imageUrl = xmlDoc.SelectSingleNode("//message").InnerText;
+        private async void GetByBreed_Click(object sender, RoutedEventArgs e)
+        {
+            if (breedList.SelectedItem == null)
+            {
+                MessageBox.Show("Please select a breed first.");
+                return;
+            }
+
+            string breed = breedList.SelectedItem.ToString();
+            string url = $"https://dog.ceo/api/breed/{breed}/images/random";
+            await FetchAndDisplayImage(url);
+        }
+
+        private async void GetBySubBreed_Click(object sender, RoutedEventArgs e)
+        {
+            if (breedList.SelectedItem == null)
+            {
+                MessageBox.Show("Please select a breed first.");
+                return;
+            }
+
+            string breed = breedList.SelectedItem.ToString();
+            string url = $"https://dog.ceo/api/breed/{breed}/list";
+            HttpResponseMessage response = await client.GetAsync(url);
+            string responseData = await response.Content.ReadAsStringAsync();
+
+            JObject json = JObject.Parse(responseData);
+            var subBreeds = json["message"].ToObject<List<string>>();
+
+            if (subBreeds.Count == 0)
+            {
+                MessageBox.Show("No sub-breeds available for this breed.");
+                return;
+            }
+
+            string subBreed = subBreeds[0];  
+            url = $"https://dog.ceo/api/breed/{breed}/{subBreed}/images/random";
+            await FetchAndDisplayImage(url);
+        }
+
+        private async void BrowseBreeds_Click(object sender, RoutedEventArgs e)
+        {
+            string url = "https://dog.ceo/api/breeds/list/all";
+            HttpResponseMessage response = await client.GetAsync(url);
+            string responseData = await response.Content.ReadAsStringAsync();
+
+            JObject json = JObject.Parse(responseData);
+            var breeds = json["message"].ToObject<Dictionary<string, List<string>>>();
+
+            breedList.Items.Clear();
+            foreach (var breed in breeds.Keys)
+            {
+                breedList.Items.Add(breed);
+            }
+        }
+
+        private async Task FetchAndDisplayImage(string url)
+        {
+            HttpResponseMessage response = await client.GetAsync(url);
+            string responseData = await response.Content.ReadAsStringAsync();
+
+            JObject json = JObject.Parse(responseData);
+            string imageUrl = json["message"].ToString();
 
             dogImage.Source = new BitmapImage(new Uri(imageUrl));
         }
